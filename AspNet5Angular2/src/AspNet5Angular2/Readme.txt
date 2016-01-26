b8ee67da17049a329927c78302123acd6db0178f
Pavel Shylan <pavel.shylan@gmail.com>
2016-01-26 17:05:17 +0200

    5min quicksteps Initial

---
+++ AspNet5Angular2/src/AspNet5Angular2/Readme.txt
@@ -0,0 +1,4 @@
+﻿Useful links:
+http://www.mithunvp.com/angular-2-in-asp-net-5-typescript-visual-studio-2015/
+
+About TypeScript: http://www.mithunvp.com/typescript-in-asp-net-5-using-visual-studio-2015/
---
+++ AspNet5Angular2/src/AspNet5Angular2/gulpfile.js
@@ -0,0 +1,24 @@
+﻿/// <binding AfterBuild='moveToLibs' />
+/*
+This file in the main entry point for defining Gulp tasks and using Gulp plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
+*/
+
+
+var gulp = require('gulp');
+
+var paths = {
+    npmSrc: "./node_modules/",
+    libTarget: "./wwwroot/libs/"
+};
+
+var libsToMove = [
+   paths.npmSrc + '/es6-shim/es6-shim.min.js',
+   paths.npmSrc + '/angular2/bundles/angular2-polyfills.js',
+   paths.npmSrc + '/systemjs/dist/system.js',
+   paths.npmSrc + '/rxjs/bundles/Rx.umd.js',
+   paths.npmSrc + '/angular2/bundles/angular2-all.umd.js'
+];
+gulp.task('moveToLibs', function () {
+    return gulp.src(libsToMove).pipe(gulp.dest(paths.libTarget));
+});
